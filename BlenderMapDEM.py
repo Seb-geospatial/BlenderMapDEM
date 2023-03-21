a40c5dbfa2a88d4070907bf11c630a2667c7cdff
Seb-B-364 <sebastianbursey@gmail.com>
2023-03-21 18:25:40 -0400

    create BlenderMapDEM.py

---
+++ BlenderMapDEM.py
@@ -0,0 +1,12 @@
+# Import required packages
+import geopandas as gpd
+
+# Fetch DEM of user specified extent
+
+# Fetch satellite imagery of user specified extent
+
+# Simplify DEM to a lower resolution
+
+# Plots 2D DEM visualization 
+
+# Generate 3D elevation map using Blender
