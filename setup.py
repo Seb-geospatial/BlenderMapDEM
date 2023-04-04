c7b3141421d0884f91ab6ba2cf4f1078c470dbe2
Seb-B-364 <sebastianbursey@gmail.com>
2023-04-03 21:03:54 -0400

    update required packages in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='BlenderMapDEM',
-    version='1.0.2',
+    version='1.0.3',
     description='A python module of functions for creating 3D elevation maps using Blender. Enables users with minimal Blender knowledge to fetch, clean, and visualize DEM data.',
     packages=find_packages(),
     install_requires=[
         'Pillow',
         'requests',
+        'rasterio',
+        'matplotlib'
     ]
 )
